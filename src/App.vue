a1cb875da9e5f4b43500f5a6b93bc54226d0ed1d
trzynasciegroszy <topdrive940@gmail.com>
2021-01-23 19:13:50 +0000

    Searchbox is working now, pics displayed

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="wrapper">
-    <HeroImage />
-    <Claim />
-    <SearchInput :value="searchValue" v-model="searchValue" @input="handleInput"/>
+    <transition name="slide">
+      <img src="./assets/logo.svg" class="logo" v-if="step === 1" />
+    </transition>
+    <transition name="fade">
+      <HeroImage v-if="step === 0" />
+    </transition>
+    <Claim v-if="step === 0" />
+    <SearchInput
+      :value="searchValue"
+      v-model="searchValue"
+      @input="handleInput"
+      :dark="step === 1"
+    />
+    <div class="results" v-if="results && !loading && step === 1">
+      <Item v-for="item in results" :item="item" :key="item.data[0].nasa_id" />
+    </div>
   </div>
 </template>
 
@@ -12,6 +25,7 @@ import debounce from "lodash.debounce";
 import Claim from "@/components/Claim";
 import SearchInput from "@/components/SearchInput";
 import HeroImage from "@/components/HeroImage";
+import Item from "@/components/Item";
 
 const API = "https://images-api.nasa.gov/search";
 
@@ -21,20 +35,26 @@ export default {
     Claim,
     SearchInput,
     HeroImage,
+    Item,
   },
   data() {
     return {
+      loading: false,
+      step: 0,
       searchValue: "",
       results: [],
     };
   },
   methods: {
     handleInput: debounce(function () {
-      console.log(this.searchValue)
+      this.loading = true;
+      console.log(this.searchValue);
       axios
         .get(`${API}?q=${this.searchValue}&media_type=image`)
         .then((response) => {
           this.results = response.data.collection.items;
+          this.loading = false;
+          this.step = 1;
         })
         .catch((error) => {
           console.log(error);
@@ -54,8 +74,25 @@ body {
   margin: 0;
   padding: 0;
 }
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+.slide-enter-active,
+.slide-leave-active {
+  transition: margin-top 0.3s ease;
+}
+.slide-enter,
+.slide-leave-to {
+  margin-top: -50px;
+}
 .wrapper {
   margin: 0;
+  position: relative;
   width: 100%;
   min-height: 100vh;
   padding: 30px;
@@ -63,5 +100,19 @@ body {
   flex-direction: column;
   align-items: center;
   justify-content: center;
+}
+.logo {
+  position: absolute;
+  top: 20px;
+}
+.results {
+  margin-top: 50px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+
+  @media (min-width: 768px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  };
 }
 </style>
